be3d3c851b83ce01e7f0f4cf8a9bf13e764901f1
John Eckersberg <jeckersb@redhat.com>
2022-10-27 09:41:53 -0400

    Use OSP17 RabbitMQ image by default instead of the upstream one

--- pkg/openstack/rabbitmq.go
+++ pkg/openstack/rabbitmq.go
@@ -7,6 +7,7 @@ import (
 	condition "github.com/openstack-k8s-operators/lib-common/modules/common/condition"
 	"github.com/openstack-k8s-operators/lib-common/modules/common/helper"
 	rabbitmqv1 "github.com/rabbitmq/cluster-operator/api/v1beta1"
+
 	// Cannot use the following import due to linting error:
 	// Error: 	pkg/openstack/rabbitmq.go:10:2: use of internal package github.com/rabbitmq/cluster-operator/internal/status not allowed
 	//rabbitstatus "github.com/rabbitmq/cluster-operator/internal/status"
@@ -37,7 +38,32 @@ func ReconcileRabbitMQ(ctx context.Context, instance *corev1beta1.OpenStackContr
 			Template: &rabbitmqv1.PodTemplateSpec{
 				Spec: &corev1.PodSpec{
 					SecurityContext: &corev1.PodSecurityContext{},
-					Containers:      []corev1.Container{},
+					Containers: []corev1.Container{
+						{
+							Name: "rabbitmq",
+							Env: []corev1.EnvVar{
+								{
+									// The upstream rabbitmq image has /var/log/rabbitmq mode 777, so when
+									// openshift runs the rabbitmq container as a random uid it can still write
+									// the logs there.  The OSP image however has the directory more constrained,
+									// so the random uid cannot write the logs there.  Force it into /var/lib
+									// where it can create the file without crashing.
+									Name:  "RABBITMQ_UPGRADE_LOG",
+									Value: "/var/lib/rabbitmq/rabbitmq_upgrade.log",
+								},
+								{
+									// For some reason HOME needs to be explictly set here even though the entry
+									// for the random user in /etc/passwd has the correct homedir set.
+									Name:  "HOME",
+									Value: "/var/lib/rabbitmq",
+								},
+							},
+							Args: []string{
+								// OSP17 runs kolla_start here, instead just run rabbitmq-server directly
+								"/usr/lib/rabbitmq/bin/rabbitmq-server",
+							},
+						},
+					},
 					InitContainers: []corev1.Container{
 						{Name: "setup-container", SecurityContext: &corev1.SecurityContext{}}},
 				},
@@ -52,7 +78,7 @@ func ReconcileRabbitMQ(ctx context.Context, instance *corev1beta1.OpenStackContr
 		// operator to continuously mutate the CR when setting it:
 		// https://github.com/rabbitmq/cluster-operator/blob/main/controllers/reconcile_operator_defaults.go#L19
 		if rabbitmq.Spec.Image == "" {
-			rabbitmq.Spec.Image = "rabbitmq:3.10.2-management"
+			rabbitmq.Spec.Image = "registry.redhat.io/rhosp-rhel9/openstack-rabbitmq:17.0"
 		}
 
 		if rabbitmq.Spec.Persistence.StorageClassName == nil {
@@ -63,6 +89,13 @@ func ReconcileRabbitMQ(ctx context.Context, instance *corev1beta1.OpenStackContr
 		if rabbitmq.Spec.Override.StatefulSet == nil {
 			helper.GetLogger().Info("Setting StatefulSet")
 			rabbitmq.Spec.Override.StatefulSet = &defaultStatefulSet
+		}
+
+		if rabbitmq.Spec.Rabbitmq.AdditionalConfig == "" {
+			helper.GetLogger().Info("Setting AdditionalConfig")
+			// This is the same situation as RABBITMQ_UPGRADE_LOG above,
+			// except for the "main" rabbitmq log we can just force it to use the console.
+			rabbitmq.Spec.Rabbitmq.AdditionalConfig = "log.console = true"
 		}
 
 		// overrides
