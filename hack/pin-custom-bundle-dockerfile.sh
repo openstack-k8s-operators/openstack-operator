58208caa2636692b4f3dfeb51d924f2389652b40
Amol Kahat <amolkahat@gmail.com>
2023-02-22 16:02:49 +0530

    Run pre-commit with make
    Updating pre-commit hook with make. pre-commit-golang
    repo is no longer maintained[1]. Instead of using
    unmaintained repo we can use make to run pre-commit.
    
    [1] https://github.com/dnephin/pre-commit-golang/issues/98

--- hack/pin-custom-bundle-dockerfile.sh
+++ hack/pin-custom-bundle-dockerfile.sh
@@ -10,27 +10,27 @@ cp custom-bundle.Dockerfile custom-bundle.Dockerfile.pinned
 
 #loop over each openstack-k8s-operators go.mod entry
 for MOD_PATH in $(go list -m -json all | jq -r '. | select(.Path | contains("openstack")) | .Replace // . |.Path' | grep -v openstack-operator | grep -v lib-common); do
-  if [[ "$MOD_PATH" == "./apis" ]]; then
-    continue
-  fi
-  MOD_VERSION=$(go list -m -json all | jq -r ". | select(.Path | contains(\"openstack\")) | .Replace // . | select( .Path == \"$MOD_PATH\") | .Version")
+    if [[ "$MOD_PATH" == "./apis" ]]; then
+        continue
+    fi
+    MOD_VERSION=$(go list -m -json all | jq -r ". | select(.Path | contains(\"openstack\")) | .Replace // . | select( .Path == \"$MOD_PATH\") | .Version")
 
-  BASE=$(echo $MOD_PATH | sed -e 's|github.com/.*/\(.*\)-operator/.*|\1|')
+    BASE=$(echo $MOD_PATH | sed -e 's|github.com/.*/\(.*\)-operator/.*|\1|')
 
-  REF=$(echo $MOD_VERSION | sed -e 's|v0.0.0-[0-9]*-\(.*\)$|\1|')
-  GITHUB_USER=$(echo $MOD_PATH | sed -e 's|github.com/\(.*\)/.*-operator/.*$|\1|')
-  REPO_CURL_URL="https://quay.io/api/v1/repository/openstack-k8s-operators"
-  REPO_URL="quay.io/openstack-k8s-operators"
-  if [[ "$GITHUB_USER" != "openstack-k8s-operators" || "$BASE" == "$IMAGEBASE" ]]; then
-      if [[ "$IMAGENAMESPACE" != "openstack-k8s-operators" || "${IMAGEREGISTRY}" != "quay.io" ]]; then
-          REPO_CURL_URL="https://${IMAGEREGISTRY}/api/v1/repository/${IMAGENAMESPACE}"
-          REPO_URL="${IMAGEREGISTRY}/${IMAGENAMESPACE}"
-      else
-          REPO_CURL_URL="https://quay.io/api/v1/repository/${GITHUB_USER}"
-          REPO_URL="quay.io/${GITHUB_USER}"
-      fi
-  fi
+    REF=$(echo $MOD_VERSION | sed -e 's|v0.0.0-[0-9]*-\(.*\)$|\1|')
+    GITHUB_USER=$(echo $MOD_PATH | sed -e 's|github.com/\(.*\)/.*-operator/.*$|\1|')
+    REPO_CURL_URL="https://quay.io/api/v1/repository/openstack-k8s-operators"
+    REPO_URL="quay.io/openstack-k8s-operators"
+    if [[ "$GITHUB_USER" != "openstack-k8s-operators" || "$BASE" == "$IMAGEBASE" ]]; then
+        if [[ "$IMAGENAMESPACE" != "openstack-k8s-operators" || "${IMAGEREGISTRY}" != "quay.io" ]]; then
+            REPO_CURL_URL="https://${IMAGEREGISTRY}/api/v1/repository/${IMAGENAMESPACE}"
+            REPO_URL="${IMAGEREGISTRY}/${IMAGENAMESPACE}"
+        else
+            REPO_CURL_URL="https://quay.io/api/v1/repository/${GITHUB_USER}"
+            REPO_URL="quay.io/${GITHUB_USER}"
+        fi
+    fi
 
-  SHA=$(curl -s ${REPO_CURL_URL}/$BASE-operator-bundle/tag/ | jq -r .tags[].name | sort -u | grep $REF)
-  sed -i custom-bundle.Dockerfile.pinned -e "s|quay.io/openstack-k8s-operators/${BASE}-operator-bundle.*|${REPO_URL}/${BASE}-operator-bundle:$SHA|"
+    SHA=$(curl -s ${REPO_CURL_URL}/$BASE-operator-bundle/tag/ | jq -r .tags[].name | sort -u | grep $REF)
+    sed -i custom-bundle.Dockerfile.pinned -e "s|quay.io/openstack-k8s-operators/${BASE}-operator-bundle.*|${REPO_URL}/${BASE}-operator-bundle:$SHA|"
 done
