4920b565aa45cca73fb262fd16b722310e5d7dfd
Harald Jensås <hjensas@redhat.com>
2023-02-23 14:34:53 +0100

    Use field.ErrorList in ValidateServices()
    Set up an error list field, and append issues to the list
    when validating service dependencies. There is no reason to
    stop on the first error.
    
    Also add checDepsEnabled for Ironic.

--- apis/core/v1beta1/openstackcontrolplane_webhook.go
+++ apis/core/v1beta1/openstackcontrolplane_webhook.go
@@ -19,7 +19,10 @@ package v1beta1
 import (
 	"fmt"
 
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
@@ -46,14 +49,40 @@ var _ webhook.Validator = &OpenStackControlPlane{}
 func (r *OpenStackControlPlane) ValidateCreate() error {
 	openstackcontrolplanelog.Info("validate create", "name", r.Name)
 
-	return r.ValidateServices()
+	var allErrs field.ErrorList
+
+	allErrs = append(allErrs, r.ValidateServices()...)
+
+	if len(allErrs) != 0 {
+		return apierrors.NewInvalid(
+			schema.GroupKind{
+				Group: "openstackcontrolplane.openstack.org",
+				Kind:  "OpenstackControlPlane",
+			},
+			r.Name, allErrs)
+	}
+
+	return nil
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (r *OpenStackControlPlane) ValidateUpdate(old runtime.Object) error {
 	openstackcontrolplanelog.Info("validate update", "name", r.Name)
 
-	return r.ValidateServices()
+	var allErrs field.ErrorList
+
+	allErrs = append(allErrs, r.ValidateServices()...)
+
+	if len(allErrs) != 0 {
+		return apierrors.NewInvalid(
+			schema.GroupKind{
+				Group: "openstackcontrolplane.openstack.org",
+				Kind:  "OpenstackControlPlane",
+			},
+			r.Name, allErrs)
+	}
+
+	return nil
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
@@ -86,44 +115,140 @@ func (r *OpenStackControlPlane) checkDepsEnabled(name string) bool {
 			r.Spec.Rabbitmq.Enabled &&
 			r.Spec.Keystone.Enabled && r.Spec.Placement.Enabled &&
 			r.Spec.Neutron.Enabled && r.Spec.Glance.Enabled)
+	case "Ironic-JsonRPC":
+		return (r.Spec.Mariadb.Enabled && r.Spec.Keystone.Enabled)
+	case "Ironic":
+		return (r.Spec.Mariadb.Enabled && r.Spec.Keystone.Enabled &&
+			r.Spec.Rabbitmq.Enabled)
 	}
 	return true
 }
 
 // ValidateServices implements common function for validating services
-func (r *OpenStackControlPlane) ValidateServices() error {
+func (r *OpenStackControlPlane) ValidateServices() field.ErrorList {
+	var allErrs field.ErrorList
+	var path *field.Path
+	var dependencies string
 
 	// Temporary check until MariaDB is deprecated
 	if r.Spec.Mariadb.Enabled && r.Spec.Galera.Enabled {
-		return fmt.Errorf("Mariadb and Galera are mutually exclusive")
+		pathA := field.NewPath("spec").Child("Mariadb").Child("enabled")
+		pathB := field.NewPath("spec").Child("Galera").Child("enabled")
+		allErrs = append(
+			allErrs,
+			field.Duplicate(
+				pathA,
+				fmt.Sprintf("%s - Mariadb and Galera are mutually exclusive", pathB.String()),
+			),
+		)
 	}
 
 	// Add service dependency validations
-	errorMsg := "%s service dependencies are not enabled."
+	errorMsg := "%s service dependencies are not enabled, needs: %s"
 
 	if r.Spec.Keystone.Enabled && !r.checkDepsEnabled("Keystone") {
-		return fmt.Errorf(errorMsg, "Keystone")
+		path = field.NewPath("spec").Child("Keystone").Child("enabled")
+		dependencies = "Mariadb"
+		allErrs = append(
+			allErrs,
+			field.Invalid(
+				path,
+				r.Spec.Keystone.Enabled,
+				fmt.Sprintf(errorMsg, "Keystone", dependencies),
+			),
+		)
 	}
 
 	if r.Spec.Glance.Enabled && !r.checkDepsEnabled("Glance") {
-		return fmt.Errorf(errorMsg, "Glance")
+		path = field.NewPath("spec").Child("Glance").Child("enabled")
+		dependencies = "Mariadb and Keystone"
+		allErrs = append(
+			allErrs,
+			field.Invalid(
+				path,
+				r.Spec.Glance.Enabled,
+				fmt.Sprintf(errorMsg, "Glance", dependencies),
+			),
+		)
 	}
 
 	if r.Spec.Cinder.Enabled && !r.checkDepsEnabled("Cinder") {
-		return fmt.Errorf(errorMsg, "Cinder")
+		path = field.NewPath("spec").Child("Cinder").Child("enabled")
+		dependencies = "Mariadb, Rabbitmq and Keystone"
+		allErrs = append(
+			allErrs,
+			field.Invalid(
+				path,
+				r.Spec.Cinder.Enabled,
+				fmt.Sprintf(errorMsg, "Cinder", dependencies),
+			),
+		)
 	}
 
 	if r.Spec.Placement.Enabled && !r.checkDepsEnabled("Placement") {
-		return fmt.Errorf(errorMsg, "Placement")
+		path = field.NewPath("spec").Child("Placement").Child("enabled")
+		dependencies = "Mariadb and Keystone"
+		allErrs = append(
+			allErrs,
+			field.Invalid(
+				path,
+				r.Spec.Placement.Enabled,
+				fmt.Sprintf(errorMsg, "Placement", dependencies),
+			),
+		)
 	}
 
 	if r.Spec.Neutron.Enabled && !r.checkDepsEnabled("Neutron") {
-		return fmt.Errorf(errorMsg, "Neutron")
+		path = field.NewPath("spec").Child("Neutron").Child("enabled")
+		dependencies = "Mariadb, Rabbitmq and Keystone"
+		allErrs = append(
+			allErrs,
+			field.Invalid(
+				path,
+				r.Spec.Neutron.Enabled,
+				fmt.Sprintf(errorMsg, "Neutron", dependencies),
+			),
+		)
 	}
 
 	if r.Spec.Nova.Enabled && !r.checkDepsEnabled("Nova") {
-		return fmt.Errorf(errorMsg, "Nova")
+		path = field.NewPath("spec").Child("Nova").Child("enabled")
+		dependencies = "Mariadb, Rabbitmq, Keystone, Placement, Neutron and Glance"
+		allErrs = append(
+			allErrs,
+			field.Invalid(
+				path,
+				r.Spec.Nova.Enabled,
+				fmt.Sprintf(errorMsg, "Nova", dependencies),
+			),
+		)
 	}
 
-	return nil
+	if r.Spec.Ironic.Enabled && !r.checkDepsEnabled("Ironic-JsonRPC") {
+		path = field.NewPath("spec").Child("Ironic").Child("enabled")
+		dependencies = "Mariadb and Keystone"
+		allErrs = append(
+			allErrs,
+			field.Invalid(
+				path,
+				r.Spec.Ironic.Enabled,
+				fmt.Sprintf(errorMsg, "Ironic", dependencies),
+			),
+		)
+	}
+
+	if (r.Spec.Ironic.Enabled && r.Spec.Ironic.Template.RPCTransport == "oslo") && !r.checkDepsEnabled("Ironic") {
+		path = field.NewPath("spec").Child("Ironic").Child("enabled")
+		dependencies = "Mariadb, Rabbitmq and Keystone"
+		allErrs = append(
+			allErrs,
+			field.Invalid(
+				path,
+				r.Spec.Ironic.Enabled,
+				fmt.Sprintf(errorMsg, "Ironic", dependencies),
+			),
+		)
+	}
+
+	return allErrs
 }
