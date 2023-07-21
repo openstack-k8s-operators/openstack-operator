b11f8195b5bd2e97f275b83961766e6e952ad79a
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2023-07-21 11:03:42 +0100

    Adding shortnames to match dataplane pattern
    https://github.com/openstack-k8s-operators/dataplane-operator/blob/main/api/v1beta1/openstackdataplane_types.go#L56C60-L56C71
    Signed-off-by: Fabricio Aguiar <fabricio.aguiar@gmail.com>

--- apis/core/v1beta1/openstackcontrolplane_types.go
+++ apis/core/v1beta1/openstackcontrolplane_types.go
@@ -478,7 +478,7 @@ type OpenStackControlPlaneStatus struct {
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
 // +operator-sdk:csv:customresourcedefinitions:displayName="OpenStack ControlPlane"
-// +kubebuilder:resource:shortName=osctlplane;osctlplanes
+// +kubebuilder:resource:shortName=osctlplane;osctlplanes;oscp;oscps
 //+kubebuilder:printcolumn:name="Status",type="string",JSONPath=".status.conditions[0].status",description="Status"
 //+kubebuilder:printcolumn:name="Message",type="string",JSONPath=".status.conditions[0].message",description="Message"
 
