f485821737611aa6043f2ea1e027dbdd85101b0b
Martin Schuppert <mschuppert@redhat.com>
2023-08-01 11:34:41 +0200

    Create glanceapi route and svc endpoint overrides
    Creates the route for the glanceapi, also allows to customize the
    route via override.
    
    Generats the service override for the env with what is configured in
    the externalEndpoints, or specified in the service template override.
    
    Depends-On: https://github.com/openstack-k8s-operators/lib-common/pull/313
    Depends-On: https://github.com/openstack-k8s-operators/keystone-operator/pull/289
    Depends-On: https://github.com/openstack-k8s-operators/glance-operator/pull/285
    
    Jira: OSP-26690

--- pkg/openstack/glance.go
+++ pkg/openstack/glance.go
@@ -6,12 +6,14 @@ import (
 
 	"github.com/openstack-k8s-operators/lib-common/modules/common/condition"
 	"github.com/openstack-k8s-operators/lib-common/modules/common/helper"
+	"github.com/openstack-k8s-operators/lib-common/modules/common/service"
 
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
 	glancev1 "github.com/openstack-k8s-operators/glance-operator/api/v1beta1"
 	corev1beta1 "github.com/openstack-k8s-operators/openstack-operator/apis/core/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
 	ctrl "sigs.k8s.io/controller-runtime"
 )
 
@@ -32,9 +34,51 @@ func ReconcileGlance(ctx context.Context, instance *corev1beta1.OpenStackControl
 		return ctrl.Result{}, nil
 	}
 
+	// Create service overrides to pass into the service CR
+	// and expose the public endpoint using a route per default.
+	// Any trailing path will be added on the service-operator level.
+	serviceOverrides := map[string]service.OverrideSpec{}
+	serviceDetails := []ServiceDetails{}
+
+	serviceOverrideSpec := map[string]service.OverrideSpec{}
+	if instance.Spec.Glance.Template.GlanceAPIExternal.Override.Service != nil {
+		serviceOverrideSpec[string(service.EndpointPublic)] = *instance.Spec.Glance.Template.GlanceAPIExternal.Override.Service
+	}
+	if instance.Spec.Glance.Template.GlanceAPIInternal.Override.Service != nil {
+		serviceOverrideSpec[string(service.EndpointInternal)] = *instance.Spec.Glance.Template.GlanceAPIInternal.Override.Service
+	}
+
+	for _, endpointType := range []service.Endpoint{service.EndpointPublic, service.EndpointInternal} {
+		sd := ServiceDetails{
+			ServiceName:         glance.Name,
+			Namespace:           instance.Namespace,
+			Endpoint:            endpointType,
+			ServiceOverrideSpec: serviceOverrideSpec,
+			RouteOverrideSpec:   instance.Spec.Glance.APIOverride.Route,
+		}
+
+		svcOverride, ctrlResult, err := sd.CreateRouteAndServiceOverride(ctx, instance, helper)
+		if err != nil {
+			return ctrlResult, err
+		} else if (ctrlResult != ctrl.Result{}) {
+			return ctrlResult, nil
+		}
+
+		serviceDetails = append(
+			serviceDetails,
+			sd,
+		)
+
+		serviceOverrides[string(endpointType)] = *svcOverride
+	}
+	instance.Status.Conditions.MarkTrue(corev1beta1.OpenStackControlPlaneServiceOverrideReadyCondition, corev1beta1.OpenStackControlPlaneServiceOverrideReadyMessage)
+
 	helper.GetLogger().Info("Reconciling Glance", "Glance.Namespace", instance.Namespace, "Glance.Name", "glance")
 	op, err := controllerutil.CreateOrPatch(ctx, helper.GetClient(), glance, func() error {
 		instance.Spec.Glance.Template.DeepCopyInto(&glance.Spec)
+		glance.Spec.GlanceAPIExternal.Override.Service = ptr.To(serviceOverrides[string(service.EndpointPublic)])
+		glance.Spec.GlanceAPIInternal.Override.Service = ptr.To(serviceOverrides[string(service.EndpointInternal)])
+
 		if glance.Spec.Secret == "" {
 			glance.Spec.Secret = instance.Spec.Secret
 		}
@@ -87,6 +131,18 @@ func ReconcileGlance(ctx context.Context, instance *corev1beta1.OpenStackControl
 			condition.RequestedReason,
 			condition.SeverityInfo,
 			corev1beta1.OpenStackControlPlaneGlanceReadyRunningMessage))
+	}
+
+	for _, sd := range serviceDetails {
+		// Add the service CR to the ownerRef list of the route to prevent the route being deleted
+		// before the service is deleted. Otherwise this can result cleanup issues which require
+		// the endpoint to be reachable.
+		// If ALL objects in the list have been deleted, this object will be garbage collected.
+		// https://github.com/kubernetes/apimachinery/blob/15d95c0b2af3f4fcf46dce24105e5fbb9379af5a/pkg/apis/meta/v1/types.go#L240-L247
+		err = sd.AddOwnerRef(ctx, helper, glance)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
 	}
 
 	return ctrl.Result{}, nil
