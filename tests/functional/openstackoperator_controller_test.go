1c976b743b40777f90ec594569ce45af258aec92
Elvira García <egarciar@redhat.com>
2023-12-20 15:41:58 +0100

    Add Neutron's default route annotations
    When the webhook calls for Neutron defaults, it should also write the
    route override annotation on Neutron's CR section.
    
    Signed-off-by: Elvira García <egarciar@redhat.com>

--- tests/functional/openstackoperator_controller_test.go
+++ tests/functional/openstackoperator_controller_test.go
@@ -96,6 +96,11 @@ var _ = Describe("OpenStackOperator controller", func() {
 				g.Expect(keystoneAPI).Should(Not(BeNil()))
 			}, timeout, interval).Should(Succeed())
 		})
+		// Default route timeouts are set
+		It("should have default timeout for the routes set", func() {
+			OSCtlplane := GetOpenStackControlPlane(names.OpenStackControlplaneName)
+			Expect(OSCtlplane.Spec.Neutron.APIOverride.Route.Annotations).Should(HaveKeyWithValue("haproxy.router.openshift.io/timeout", "120s"))
+		})
 
 		It("should create selfsigned issuer and public+internal CA and issuer", func() {
 			OSCtlplane := GetOpenStackControlPlane(names.OpenStackControlplaneName)
