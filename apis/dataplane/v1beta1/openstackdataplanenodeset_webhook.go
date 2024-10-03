e1116fd321eec48f2c3d84ace71b7d05ee19298b
rabi <ramishra@redhat.com>
2024-10-03 20:29:58 +0530

    Remove PreprovisioningNetworkDataName
    jira: https://issues.redhat.com/browse/OSPRH-10442
    
    Signed-off-by: rabi <ramishra@redhat.com>

--- apis/dataplane/v1beta1/openstackdataplanenodeset_webhook.go
+++ apis/dataplane/v1beta1/openstackdataplanenodeset_webhook.go
@@ -92,7 +92,6 @@ func (spec *OpenStackDataPlaneNodeSetSpec) Default() {
 			instanceSpec := baremetalv1.InstanceSpec{}
 			instanceSpec.UserData = node.UserData
 			instanceSpec.NetworkData = node.NetworkData
-			instanceSpec.PreprovisioningNetworkDataName = node.PreprovisioningNetworkDataName
 			nodeSetHostMap[node.HostName] = instanceSpec
 		}
 		spec.BaremetalSetTemplate.BaremetalHosts = nodeSetHostMap
