5a475934d95f5cab925fb9002ffddeaea4c8c5aa
Brendan Shephard <bshephar@redhat.com>
2024-11-08 09:12:34 +1000

    Require EDPMServiceType
    This change switches EDPMServiceType from optional to required.
    Since the EDPMServiceType of custom services needs to match that of an existing service,
    we also remove the defaulting mechanism in the webhook.
    
    Signed-off-by: Brendan Shephard <bshephar@redhat.com>

--- apis/dataplane/v1beta1/openstackdataplaneservice_types.go
+++ apis/dataplane/v1beta1/openstackdataplaneservice_types.go
@@ -53,7 +53,7 @@ type OpenstackDataPlaneServiceCert struct {
 	// not set, OpenStackDataPlaneService.Spec.EDPMServiceType is used. If
 	// OpenStackDataPlaneService.Spec.EDPMServiceType is not set, then
 	// OpenStackDataPlaneService.Name is used.
-	EDPMRoleServiceName string `json:"edpmRoleServiceName,omitempty"`
+	EDPMRoleServiceName string `json:"edpmRoleServiceName"`
 }
 
 // OpenStackDataPlaneServiceSpec defines the desired state of OpenStackDataPlaneService
@@ -111,7 +111,8 @@ type OpenStackDataPlaneServiceSpec struct {
 	// corresponds to the ansible role name (without the "edpm_" prefix) used
 	// to manage the service. If not set, will default to the
 	// OpenStackDataPlaneService name.
-	EDPMServiceType string `json:"edpmServiceType,omitempty" yaml:"edpmServiceType,omitempty"`
+	// +kubebuilder:validation:Required
+	EDPMServiceType string `json:"edpmServiceType" yaml:"edpmServiceType"`
 }
 
 // OpenStackDataPlaneServiceStatus defines the observed state of OpenStackDataPlaneService
